a6fbbf73d228784910be63872f8cdd22c4f50e26
zhanhui913 <zhanhui913@gmail.com>
2017-11-12 16:13:10 -0800

    First commit for swift code

---
+++ Budget/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Budget
+//
+//  Created by Zhan H. Yap on 2017-11-12.
+//  Copyright © 2017 Zhan H. Yap. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Budget/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Budget
+//
+//  Created by Zhan H. Yap on 2017-11-12.
+//  Copyright © 2017 Zhan H. Yap. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
