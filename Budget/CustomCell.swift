4e2242ca93b655ee4868e70176dc22cbe19bd887
zhanhui913 <zhanhui913@gmail.com>
2017-11-13 15:19:12 -0800

    Finally added calendar view to 4th view controller

---
+++ Budget/CustomCell.swift
@@ -0,0 +1,14 @@
+//
+//  CustomCell.swift
+//  Budget
+//
+//  Created by Zhan H. Yap on 2017-11-13.
+//  Copyright © 2017 Zhan H. Yap. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class CustomCell: JTAppleCell {
+    @IBOutlet weak var dateLabel : UILabel!
+}
--- Budget/FourthViewController.swift
+++ Budget/FourthViewController.swift
@@ -1,35 +1,83 @@
 //
-//  FourthViewController.swift
+//  FirstViewController.swift
 //  Budget
 //
 //  Created by Zhan H. Yap on 2017-11-12.
 //  Copyright © 2017 Zhan H. Yap. All rights reserved.
 //
 
-import UIKit
+import JTAppleCalendar
+import os.log
 
 class FourthViewController: UIViewController {
-
+    let formatter = DateFormatter()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        os_log("fourth view controller here", log: OSLog.default, type: .info)
 
-        // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
+
+extension FourthViewController : JTAppleCalendarViewDelegate, JTAppleCalendarViewDataSource {
+    func calendar(_ calendar: JTAppleCalendarView, willDisplay cell: JTAppleCell, forItemAt date: Date, cellState: CellState, indexPath: IndexPath) {
+        os_log("fourth view controller here 2", log: OSLog.default, type: .info)
+    }
+    
+    func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
+        /*
+        formatter.dateFormat = "yyyy MM dd"
+        formatter.timeZone = Calendar.current.timeZone
+        formatter.locale = Calendar.current.locale
+        
+        let startDate = formatter.date(from: "2017 11 01")! // You can use date generated from a formatter
+        let endDate = Date()                                // You can also use dates created from this function
+        let calendar = Calendar.current                     // Make sure you set this up to your time zone. We'll just use default here
+        
+        let parameters = ConfigurationParameters(startDate: startDate,
+                                                 endDate: endDate,
+                                                 numberOfRows: 6,
+                                                 calendar: calendar,
+                                                 generateInDates: .forAllMonths,
+                                                 generateOutDates: .tillEndOfGrid,
+                                                 firstDayOfWeek: .sunday)*/
+        formatter.dateFormat = "yyyy MM dd"
+        formatter.timeZone = Calendar.current.timeZone
+        formatter.locale = Calendar.current.locale
+        
+        os_log("fourth view controller here 3", log: OSLog.default, type: .info)
+        
+        let startDate = formatter.date(from: "2017 01 01")!
+        let endDate = formatter.date(from: "2017 12 31")!
+        let parameters = ConfigurationParameters(startDate: startDate, endDate: endDate)
+        
+        return parameters
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTAppleCell {
+        let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "CustomCell", for: indexPath) as! CustomCell
+        cell.dateLabel.text = cellState.text
+        return cell
+    }
+    
+}
+
+extension UIColor {
+    convenience init(colorWithHexValue value: Int, alpha:CGFloat = 1.0){
+        self.init(
+            red: CGFloat((value & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((value & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(value & 0x0000FF) / 255.0,
+            alpha: alpha
+        )
+    }
+}
+
+
+
