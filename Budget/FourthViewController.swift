cc1ee2f8f351a630f953baa80c211d91313118c0
zhanhui913 <zhanhui913@gmail.com>
2017-11-18 11:24:22 -0800

    Added calendar ability to select date, change selected date color, and change outside and inside month text color

--- Budget/FourthViewController.swift
+++ Budget/FourthViewController.swift
@@ -12,20 +12,57 @@ import os.log
 class FourthViewController: UIViewController {
     let formatter = DateFormatter()
     
+    @IBOutlet weak var calendarView: JTAppleCalendarView!
+    
+    
+    let outsideMonthTextColor = UIColor(colorWithHexValue : 0x584A66)
+    let thisMonthTextColor = UIColor.white
+    let selectedMonthColor = UIColor(colorWithHexValue : 0x3A294B)
+    let currentDateSelectedViewColor = UIColor(colorWithHexValue : 0x4E3F5B)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         os_log("fourth view controller here", log: OSLog.default, type: .info)
-
+        setupCalendarView()
     }
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func setupCalendarView(){
+        calendarView.minimumLineSpacing = 0
+        //removes the individual cell insets
+        calendarView.minimumInteritemSpacing = 0
+    }
+    
+    func handleCellTextSelected(view : JTAppleCell?, cellState : CellState){
+        guard let validCell = view as? CustomCell else { return }
+        if validCell.isSelected {
+            validCell.selectedView.isHidden = false
+        } else {
+            validCell.selectedView.isHidden = true
+        }
+    }
+    
+    func handleCellTextColor(view : JTAppleCell?, cellState : CellState){
+        guard let validCell = view as? CustomCell else { return }
+        if cellState.isSelected {
+            validCell.dateLabel.textColor = selectedMonthColor
+        } else {
+            if cellState.dateBelongsTo == .thisMonth {
+                validCell.dateLabel.textColor = thisMonthTextColor
+            } else {
+                validCell.dateLabel.textColor = outsideMonthTextColor
+            }
+        }
+    }
 }
 
 
-extension FourthViewController : JTAppleCalendarViewDelegate, JTAppleCalendarViewDataSource {
+extension FourthViewController : JTAppleCalendarViewDelegate {
     func calendar(_ calendar: JTAppleCalendarView, willDisplay cell: JTAppleCell, forItemAt date: Date, cellState: CellState, indexPath: IndexPath) {
         os_log("fourth view controller here 2", log: OSLog.default, type: .info)
     }
@@ -60,13 +97,37 @@ extension FourthViewController : JTAppleCalendarViewDelegate, JTAppleCalendarVie
         return parameters
     }
     
+ 
+    
+}
+
+
+extension FourthViewController : JTAppleCalendarViewDataSource {
+    //Display the cell
     func calendar(_ calendar: JTAppleCalendarView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTAppleCell {
         let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "CustomCell", for: indexPath) as! CustomCell
         cell.dateLabel.text = cellState.text
+        
+        handleCellTextSelected(view : cell, cellState : cellState)
+        handleCellTextColor(view : cell, cellState : cellState)
         return cell
     }
     
+    func calendar(_ calendar: JTAppleCalendarView, didSelectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
+        handleCellTextSelected(view : cell, cellState : cellState)
+        handleCellTextColor(view : cell, cellState : cellState)
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, didDeselectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
+        handleCellTextSelected(view : cell, cellState : cellState)
+        handleCellTextColor(view : cell, cellState : cellState)
+    }
 }
+
+
+
+
+
 
 extension UIColor {
     convenience init(colorWithHexValue value: Int, alpha:CGFloat = 1.0){
