293b20280a705154dc672c464e5b426cfa49289d
zhanhui913 <zhanhui913@gmail.com>
2017-11-26 14:41:22 -0800

    Added tableViewController

---
+++ Budget/TransactionTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  TransactionTableViewCell.swift
+//  Budget
+//
+//  Created by Zhan H. Yap on 2017-11-26.
+//  Copyright © 2017 Zhan H. Yap. All rights reserved.
+//
+
+import UIKit
+
+class TransactionTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var transactionName : UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
