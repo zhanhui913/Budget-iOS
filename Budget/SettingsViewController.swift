baed2209f9b975be2daabee110358f98245ebbe4
zhanhui913 <zhanhui913@gmail.com>
2017-12-04 20:42:23 -0800

    Added static table view cells for settings page

--- Budget/SettingsViewController.swift
+++ Budget/SettingsViewController.swift
@@ -8,28 +8,23 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
-
+class SettingsTableViewController: UITableViewController, UITableViewDelegate {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.tableView.backgroundColor = UIColor(red: 243.0/255, green: 243.0/255, blue: 243.0/255, alpha: 1)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func tableView(tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        let headerView = view as UITableViewHeaderFooterView
+        headerView.textLabel.textColor = UIColor(red: 151.0/255, green: 193.0/255, blue: 100.0/255, alpha: 1)
+        let font = UIFont(name: "Montserrat", size: 18.0)
+        headerView.textLabel.font = font!
     }
-    */
-
+    
 }
